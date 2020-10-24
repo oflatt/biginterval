5f9fa90af1ea57c86e0ed45446db9a5adc84c288
oliver <oflatt@gmail.com>
2020-10-24 10:47:08 -0600

    add a bunch of functions

--- mpfi.rkt
+++ mpfi.rkt
@@ -42,14 +42,52 @@
       y)
     (provide name)))
 
+(define-syntax-rule (provide-1ary-fun name c-name)
+  (begin
+    (define cfun
+      (get-mpfi-fun c-name (_fun _mpfi-pointer _mpfi-pointer -> _int)))
+    (define (name x1)
+      (define y (new-mpfi 0 0))
+      (cfun y x1)
+      y)
+    (provide name)))
+
+
 (define-syntax-rule (provide-2ary-funs [name c-name] ...)
   (begin (provide-2ary-fun name c-name) ...))
+(define-syntax-rule (provide-1ary-funs [name c-name] ...)
+  (begin (provide-1ary-fun name c-name) ...))
 
 (provide-2ary-funs
   [ivadd 'mpfi_add]
   [ivsub 'mpfi_sub]
   [ivmul 'mpfi_mul]
-  [ivdiv 'mpfi_div])
+  [ivdiv 'mpfi_div]
+  [ivatan2  'mpfi_atan2]
+  [ivhypot  'mpfi_hypot])
+
+(provide-1ary-funs
+  [ivneg 'mpfi_neg]
+  [ivacos 'mpfi_acos]
+  [ivsin 'mpfi_sin]
+  [ivasin 'mpfi_asin]
+  [ivasinh 'mpfi_asinh]
+  [ivtan    'mpfi_tan]
+  [ivatan   'mpfi_atan]
+  [ivatanh   'mpfi_atanh]
+  [ivcbrt    'mpfi_cbrt]
+  [ivcos     'mpfi_cos]
+  [ivcosh    'mpfi_cosh]
+  [ivexp     'mpfi_exp]
+  [ivexp2    'mpfi_exp2]
+  [ivexpm1    'mpfi_expm1]
+  [ivfabs     'mpfi_abs]
+  [ivlog       'mpfi_log]
+  [ivlog10     'mpfi_log10]
+  [ivlog1p   'mpfi_log1p]
+  [ivlog2    'mpfi_log2]
+  [ivsqrt     'mpfi_sqrt])
+
 
 (define-syntax-rule (provide-mpfr-mpfi-fun name c-name)
   (begin
@@ -64,5 +102,8 @@
 (provide-mpfr-mpfi-fun ivleft 'mpfi_get_left)
 (provide-mpfr-mpfi-fun ivright 'mpfi_get_right)
 
+
 (ivleft (ivadd (iv 1 2) (iv 3 4)))
 (ivright (ivadd (iv 1 2) (iv 3 4)))
+(ivleft (ivsin (iv 0 1)))
+(ivright (ivsin (iv 0 1)))
